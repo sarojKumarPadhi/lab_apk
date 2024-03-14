5f1e5d799195be689e8c3a0258d4760501abb256
abhishekedugaon <abhishekkumar.edugaon@gmail.com>
2024-03-14 13:27:45 +0530

    show rider data on lab rider screen

--- lib/page/after_ride_accept_by_rider.dart
+++ lib/page/after_ride_accept_by_rider.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../global/progressIndicator.dart';
 import '../services/networkRequest.dart';
 
@@ -31,6 +34,8 @@ class _AfterAcceptanceRidePageState extends State<AfterAcceptanceRidePage> {
   BitmapDescriptor? customMarkerRiderIcon;
   bool dataLoaded = false;
   String rideStatus = "accept";
+  String rideName = " ";
+  String ridePhone = " ";
   Set<Marker> setOfMarkers = <Marker>{};
   StreamSubscription<DatabaseEvent>? _driverLocationSubscription;
 
@@ -94,21 +99,19 @@ class _AfterAcceptanceRidePageState extends State<AfterAcceptanceRidePage> {
   }
 
   Future<void> getDataFromRealtime() async {
-    final dataSnapshot = await FirebaseDatabase.instance
-        .ref()
+    final dataSnapshot = await FirebaseDatabase.instance.ref()
         .child("active_labs/${widget.labUid}/${widget.requestId}")
         .once();
 
     if (dataSnapshot.snapshot.value != null) {
       final dataSnap = dataSnapshot.snapshot.value as Map<dynamic, dynamic>;
 
-      riderLatLng = LatLng(dataSnap["driverInitialLocation"]["initialLatitude"],
-          dataSnap["driverInitialLocation"]["initialLongitude"]);
-      labLatLng = LatLng(dataSnap["labDetails"]["latitude"],
-          dataSnap["labDetails"]["longitude"]);
-      patientLatLng = LatLng(dataSnap["patientDetails"]["latitude"],
-          dataSnap["patientDetails"]["longitude"]);
+      riderLatLng = LatLng(dataSnap["driverInitialLocation"]["initialLatitude"], dataSnap["driverInitialLocation"]["initialLongitude"]);
+      labLatLng = LatLng(dataSnap["labDetails"]["latitude"], dataSnap["labDetails"]["longitude"]);
+      patientLatLng = LatLng(dataSnap["patientDetails"]["latitude"], dataSnap["patientDetails"]["longitude"]);
       rideStatus = dataSnap["rideStatus"];
+      rideName = dataSnap["riderDetails"]["riderName"];
+      ridePhone = dataSnap["riderDetails"]["riderPhone"];
       dataLoaded = true;
 
       getPolyline();
@@ -155,29 +158,97 @@ class _AfterAcceptanceRidePageState extends State<AfterAcceptanceRidePage> {
         },
         child: const Icon(Icons.refresh),
       ),
-      body: dataLoaded
-          ? GoogleMap(
-              initialCameraPosition: riderLatLng != null
-                  ? CameraPosition(target: riderLatLng!, zoom: 14)
-                  : const CameraPosition(target: LatLng(0, 0)),
-              polylines: polyLineSet,
-              markers: {
-                Marker(
-                  markerId: const MarkerId('lab_location'),
-                  position: labLatLng!,
-                  infoWindow: const InfoWindow(title: 'Lab Location'),
-                  icon: customMarkerLabIcon!,
+      body:Stack(
+        children: [
+          dataLoaded
+              ? GoogleMap(
+            initialCameraPosition: riderLatLng != null
+                ? CameraPosition(target: riderLatLng!, zoom: 14)
+                : const CameraPosition(target: LatLng(0, 0)),
+            polylines: polyLineSet,
+            markers: {
+              Marker(
+                markerId: const MarkerId('lab_location'),
+                position: labLatLng!,
+                infoWindow: const InfoWindow(title: 'Lab Location'),
+                icon: customMarkerLabIcon!,
+              ),
+              Marker(
+                markerId: const MarkerId('patient_location'),
+                position: patientLatLng!,
+                infoWindow: const InfoWindow(title: 'Patient Location'),
+                icon: customMarkerPatientIcon!,
+              ),
+              ...setOfMarkers
+            },
+          )
+              : const Center(child: CircularProgress()),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(15),topRight:Radius.circular(15)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.white30,
+                        blurRadius: 18,
+                        spreadRadius: .5,
+                        offset: Offset(0.6, 0.6))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 8,
+                    ),
+                        Row(
+                       children: [
+                         Padding(
+                           padding: const EdgeInsets.only(top: 10,bottom: 10,right: 10),
+                           child: Icon(Icons.directions_bike_outlined,color: Colors.white,),
+                         ),
+                        Text(
+                          rideName,
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.lightGreenAccent),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: IconButton(
+                            onPressed: () {
+                              _launchPhoneCall(ridePhone);
+                            },
+                            icon: Icon(Icons.phone_android_outlined,color: Colors.white70,),
+                          ),
+                        )
+                      ],
+                    ),
+
+                  ],
                 ),
-                Marker(
-                  markerId: const MarkerId('patient_location'),
-                  position: patientLatLng!,
-                  infoWindow: const InfoWindow(title: 'Patient Location'),
-                  icon: customMarkerPatientIcon!,
-                ),
-                ...setOfMarkers
-              },
-            )
-          : const Center(child: CircularProgress()),
+              ),
+            ),
+          )
+
+        ],
+      )
     );
+  }
+  /// create function to calling
+  Future<void> _launchPhoneCall(String phoneNumber) async {
+    final Uri phoneCallUri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunch(phoneCallUri.toString())) {
+      await launch(phoneCallUri.toString());
+    }
+    else {
+      throw 'Could not launch $phoneCallUri';
+    }
   }
 }
