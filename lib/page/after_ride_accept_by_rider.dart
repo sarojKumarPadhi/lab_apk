2295ccb735f7c7a58341b39e783112524771c5f8
abhishekedugaon <abhishekkumar.edugaon@gmail.com>
2024-03-15 17:34:00 +0530

    letest code

--- lib/page/after_ride_accept_by_rider.dart
+++ lib/page/after_ride_accept_by_rider.dart
@@ -119,9 +119,9 @@ class _AfterAcceptanceRidePageState extends State<AfterAcceptanceRidePage> {
       riderName = dataSnap["riderDetails"]["riderName"];
       riderPhone = dataSnap["riderDetails"]["riderPhone"];
       dataLoaded = true;
-
       getPolyline();
-    } else {
+    }
+    else {
       Fluttertoast.showToast(msg: "Data not found");
     }
   }
